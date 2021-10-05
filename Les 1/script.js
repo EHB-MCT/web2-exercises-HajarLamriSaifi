7439b98cf614e51227c6bf420ceb1433d1d1f0ea
HajarLamriSaifi <56121110+HajarLamriSaifi@users.noreply.github.com>
2021-10-05 21:09:08 +0200

    pokemon
    api fetch pokemon

---
+++ Les 1/script.js
@@ -0,0 +1,16 @@
+
+
+window.onload = () => {
+    console.log('loaded');
+
+    document.getElementById()
+    .addEventListener('submit', event => {
+        event.preventDefault();
+
+        let name = document.getElementById('nameinput').value;
+        let email = document.getElementById('emailinput').value;
+        let order = document.getElementById('orderinput').value;
+        
+    })
+
+  };
---
+++ les2/script.js
@@ -0,0 +1,30 @@
+import Team from './team.js'
+
+let team1 = new Team 
+
+let list = [];
+let pokemons= [];
+fetch('https://pokeapi.co/api/v2/pokemon?limit=151')
+.then(resp => resp.json())
+.then (data => {
+    list = data.results;
+    // LOOP OVER EVERY POKEMON
+    list.forEach(poke =>{
+        fetch(poke.url)
+        .then (resp => resp.json())
+        .then(pokeObject => {
+            pokemons.push(pokeObject);
+
+        });
+    });
+
+});
+
+window.onload = function(){
+    setTimeout (buildList, 3000);
+
+    function buildList(){
+        console.log(pokemons);
+      
+    }   
+}
