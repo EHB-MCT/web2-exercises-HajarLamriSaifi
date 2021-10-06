7830ca3842891453ff08b78abcc79c87144ee8cb
HajarLamriSaifi <56121110+HajarLamriSaifi@users.noreply.github.com>
2021-10-06 14:42:48 +0200

    web2
    les1

--- Les 1/script.js
+++ Les 1/script.js
@@ -1,9 +1,30 @@
 
+const dishes = [];
+
+dishes.push({
+id: '1',
+name: 'Burger and french fries',
+price: '18',
+
+});
+
+dishes.push({
+id: '2',
+name: 'Steak and salade',
+price: '30',
+})
+
+dishes.push({
+id: '3',
+name: 'Falafel',
+price: '5',
+})
+
 
 window.onload = () => {
     console.log('loaded');
 
-    document.getElementById()
+    document.getElementById('form')
     .addEventListener('submit', event => {
         event.preventDefault();
 
@@ -11,6 +32,25 @@ window.onload = () => {
         let email = document.getElementById('emailinput').value;
         let order = document.getElementById('orderinput').value;
         
+        let orders = {
+            name,
+            email,
+            order,
+            
+        }
+
+
+    function printOrder(name, email, order) {
+        let strings = `The order for the customer ${name} is the following: ${order}
+        The customer may be notified by email: ${email}`
+
+        document.getElementById('message').innerHTML= strings;
+    }
+        
+    printOrder(orders.name, orders.email, orders.order);
+        
+
     })
 
   };
+
